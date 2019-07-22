859a87467fd0f87781f682e2ab77a6702f77aa64
ker <20141336@ce.pucmm.edu.do>
2019-07-22 19:35:57 -0400

    Proyecto inicializado

---
+++ src/main/java/arkham/knight/practica13/Practica13Application.java
@@ -0,0 +1,13 @@
+package arkham.knight.practica13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Practica13Application.class, args);
+    }
+
+}
